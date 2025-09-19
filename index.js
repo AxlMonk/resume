8be85d027ad913dea048b3bb9ae343fee997536b
Noa <aakngrv@mail.ru>
2025-09-20 04:05:41 +0500

    tabs ok

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-import { setNavBorder, setSmoothScroll } from "./src/scripts/main.js";
+import {setNavBorder, setNavItems, setNavLinks, setSmoothScroll} from "./src/scripts/main.js";
 import './src/styles/main.css';
+import './src/styles/about.css';
 
 setNavBorder();
 setSmoothScroll();
+setNavItems();
+setNavLinks();
 
 
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,37 @@
 export function setNavBorder() {
   const mainNavList = document.getElementById('mainNavList');
-  const mainNavItems = mainNavList.querySelectorAll('li');
+  const mainNavItems = mainNavList.querySelectorAll('.main-header__item--link');
   mainNavItems.forEach(item => {
     item.addEventListener('click', function() {
+      const selectedItemId = item.id;
+      localStorage.setItem('activeMenuItem', selectedItemId);
       mainNavItems.forEach(li => li.classList.remove('activated'));
       this.classList.add('activated');
+    });
+  });
+}
+
+export function setNavItems() {
+  window.addEventListener('load', () => {
+    const activeMenuItemId = localStorage.getItem('activeMenuItem');
+    if (activeMenuItemId) {
+      const activeItem = document.getElementById(activeMenuItemId);
+      if (activeItem) {
+        activeItem.classList.add('activated');
+      }
+    }
+  });
+}
+
+export function setNavLinks() {
+  window.addEventListener('scroll', () => {
+    document.querySelectorAll('section').forEach((sec) => {
+      let menuLink = document.querySelector(`.main-header__item--link[href="#${sec.id}"]`);
+      if (window.scrollY >= sec.offsetTop && window.scrollY < sec.offsetTop + sec.offsetHeight) {
+        menuLink.classList.add('activated');
+      } else {
+        menuLink.classList.remove('activated');
+      }
     });
   });
 }
@@ -19,9 +46,9 @@ export function setSmoothScroll() {
 
       if (targetElement) {
         const startPosition = window.scrollY; // Начальная позиция скролла
-        const targetPosition = targetElement.getBoundingClientRect().top + window.scrollY; // Позиция цели
-        const distance = targetPosition - startPosition; // Общая дистанция для прокрутки
-        const duration = 600; // Длительность анимации в миллисекундах
+        const targetPosition = targetElement.getBoundingClientRect().top + window.scrollY;
+        const distance = targetPosition - startPosition;
+        const duration = 600;
         let startTime = null;
 
         function animation(currentTime) {
@@ -29,18 +56,17 @@ export function setSmoothScroll() {
           const timeElapsed = currentTime - startTime;
           const progress = Math.min(timeElapsed / duration, 1); // Прогресс анимации от 0 до 1
 
-          // Вычисляем текущую позицию прокрутки с использованием easing-функции
           window.scrollTo(0, startPosition + distance * easeInOutQuad(progress));
 
           if (progress < 1) {
             requestAnimationFrame(animation); // Запланировать следующий кадр
           }
         }
-        // Простая easing-функция (например, easeInOutQuad)
+
         function easeInOutQuad(t) {
           return t < .5 ? 2 * t * t : 1 - Math.pow(-2 * t + 2, 2) / 2;
         }
-        requestAnimationFrame(animation); // Начать анимацию
+        requestAnimationFrame(animation);
       }
     });
   });
